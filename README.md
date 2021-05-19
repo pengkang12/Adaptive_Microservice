7728e7e79597d0dbe068384c85cf66b5a7d140a3
tutu <123kangpeng@gmail.com>
2021-05-19 18:28:48 -0500

    update locust workload

--- README.md
+++ README.md
@@ -3,4 +3,22 @@
 # runtine environment
 python3.6
 
+# check environment
+locust version is 1.5.1
+
+this ip address is the api to access  the Robot-shop application. http://10.105.207.202:8080 
+```
+kubectl get service -n robot-shop
+NAME        TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)              AGE
+web         ClusterIP   10.105.207.202   <none>        8080/TCP             10d
+```
+
+```
+locust --host http://10.105.207.202:8080 -f robot-shop-2.py -u 10 -t 110s --print-stats --csv=locust  --headless 
+```
+
+# how to run program
+
+bash run.sh
+
 
--- robot-shop.py
+++ robot-shop.py
@@ -1,133 +1,96 @@
 import os
-from locust import TaskSet, task, HttpUser, between
+import random
+
+from locust import HttpUser, task, between
 from random import choice
 from random import randint
-import time
 
-def random_delay():
-    time.sleep(1)
+class UserBehavior(HttpUser):
+    wait_time = between(2, 10)
 
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 50)
- 
+    # source: https://tools.tracemyip.org/search--ip/list
+    fake_ip_addresses = [
+        # white house
+        "156.33.241.5",
+        # Hollywood
+        "34.196.93.245",
+        # Chicago
+        "98.142.103.241",
+        # Los Angeles
+        "192.241.230.151",
+        # Berlin
+        "46.114.35.116",
+        # Singapore
+        "52.77.99.130",
+        # Sydney
+        "60.242.161.215"
+    ]
+
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
         print('Starting')
 
     @task
     def login(self):
+        fake_ip = random.choice(self.fake_ip_addresses)
+
         credentials = {
                 'name': 'user',
                 'password': 'password'
                 }
-        res = self.client.post('/api/user/login', json=credentials, name="/api/user/login")
-        #except:
-        #    print("Error generated")
+        res = self.client.post('/api/user/login', json=credentials, headers={'x-forwarded-for': fake_ip})
         print('login {}'.format(res.status_code))
-    
+
+
     @task
     def load(self):
-        self.client.get('/')
-        user = self.client.get('/api/user/uniqueid', name="/api/user/uniqueid").json()
+        fake_ip = random.choice(self.fake_ip_addresses)
+
+        self.client.get('/', headers={'x-forwarded-for': fake_ip})
+        user = self.client.get('/api/user/uniqueid', headers={'x-forwarded-for': fake_ip}).json()
         uniqueid = user['uuid']
         print('User {}'.format(uniqueid))
 
-        result = self.client.get('/api/catalogue/categories',name="/api/catalogue/categories/")
-        response = self.client.get('/api/catalogue/products',name="/api/catalogue/product/")
-        products = response.json() 
-        for i in range(8):
-            item = choice(products)
+        self.client.get('/api/catalogue/categories', headers={'x-forwarded-for': fake_ip})
+        # all products in catalogue
+        products = self.client.get('/api/catalogue/products', headers={'x-forwarded-for': fake_ip}).json()
+        for i in range(2):
+            item = None
             while True:
                 item = choice(products)
                 if item['instock'] != 0:
                     break
-                response = self.client.get('/api/catalogue/product/{}'.format(item['sku']),name="/api/catalogue/product")
-                if response.status_code in [500, 502]:
-                    time.sleep(2)
+
             # vote for item
+            if randint(1, 10) <= 3:
+                self.client.put('/api/ratings/api/rate/{}/{}'.format(item['sku'], randint(1, 5)), headers={'x-forwarded-for': fake_ip})
 
-            print("item ", item)
-            if randint(1, 20) <= 1:
-                response = self.client.put('/api/ratings/api/rate/{}/{}'.format(item['sku'], randint(1, 5)), name="/api/ratings/api/rate")
-                if response.status_code in [500, 502]:
-                    time.sleep(2)
-            for i in range(20):
-                response = self.client.get('/api/catalogue/product/{}'.format(item['sku']),name="/api/catalogue/product/")
-                if response.status_code in [500, 502]:
-                    time.sleep(2)
-	
-            for i in range(5): 
-                response = self.client.get('/api/ratings/api/fetch/{}'.format(item['sku']),name="/api/ratings")
-                if  response.status_code in [500, 502]:
-                    time.sleep(2)
+            self.client.get('/api/catalogue/product/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
+            self.client.get('/api/ratings/api/fetch/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
+            self.client.get('/api/cart/add/{}/{}/1'.format(uniqueid, item['sku']), headers={'x-forwarded-for': fake_ip})
 
-                response = self.client.get('/api/cart/add/{}/{}/1'.format(uniqueid, item['sku']),name="/api/cart/add")
-                if response.status_code in [500, 502]:
-                    time.sleep(2)
-	
-        response  = self.client.get('/api/cart/cart/{}'.format(uniqueid),name="/api/cart/cart")
-        cart = None
-        if  response.status_code in [500, 502]:
-            time.sleep(3)
-            response = self.client.get('/api/cart/cart/{}'.format(uniqueid),name="/api/cart/cart")
-            if  response.status_code in [500, 502]:
-                time.sleep(3)
-                return 	
-            cart = response.json() 
-        cart = response.json()
+        cart = self.client.get('/api/cart/cart/{}'.format(uniqueid), headers={'x-forwarded-for': fake_ip}).json()
         item = choice(cart['items'])
-        response = self.client.get('/api/cart/update/{}/{}/2'.format(uniqueid, item['sku']),name="/api/cart/update")
-        if response.status_code in [500, 502]:
-            time.sleep(3)
+        self.client.get('/api/cart/update/{}/{}/2'.format(uniqueid, item['sku']), headers={'x-forwarded-for': fake_ip})
+
         # country codes
-        if randint(1, 8) <= 8:
-            response = self.client.get('/api/shipping/codes', name="/api/shipping/codes")
-            if  response.status_code in [500, 502]:
-                time.sleep(3)
-                return  		
-            code = choice(response.json())
-            city = None
-            if randint(1, 2) <= 1:
-                response = self.client.get('/api/shipping/cities/{}'.format(code['code']), name="/api/shipping/cites")
-                if response.status_code in [500, 502]:
-                    time.sleep(3)
-                    city = {'uuid': 5381355, 'name': 'Olariu'}
-                else:
-                    city = choice(response.json())
-            else:
-                city = {'uuid': 5381355, 'name': 'Olariu'}
-            print('code {} city {}'.format(code, city))
-            response = self.client.get('/api/shipping/calc/{}'.format(city['uuid']), name="/api/shipping/calc") 
-            if response.status_code in [500, 502]:
-                time.sleep(3)
-                shipping = {'distance': 7360, 'cost': 368.0, 'location': 'Canada Skookumchuck'}
-            else:
-                shipping = response.json()
-        else:
-            shipping = {'distance': 7360, 'cost': 368.0, 'location': 'Canada Skookumchuck'}
-        print('Shipping {}'.format(shipping)) 
- 	# POST
-        if randint(1, 15) <= 15:
-            response = self.client.post('/api/shipping/confirm/{}'.format(uniqueid), json=shipping, name="/api/shipping/confirm") 
-            if response.status_code in [500, 502]:
-                time.sleep(3)
-                return 
-            cart = response.json()
-            print('Final cart {}'.format(cart))
-            if randint(1, 3) <= 3:
-                response = self.client.post('/api/payment/pay/{}'.format(uniqueid), json=cart, name="/api/payment/pay")
-                if response.status_code in [500, 502]:
-                    time.sleep(3)
-                    return 
-                order = response.json()
-                print('Order {}'.format(order))
+        code = choice(self.client.get('/api/shipping/codes', headers={'x-forwarded-for': fake_ip}).json())
+        city = choice(self.client.get('/api/shipping/cities/{}'.format(code['code']), headers={'x-forwarded-for': fake_ip}).json())
+        print('code {} city {}'.format(code, city))
+        shipping = self.client.get('/api/shipping/calc/{}'.format(city['uuid']), headers={'x-forwarded-for': fake_ip}).json()
+        shipping['location'] = '{} {}'.format(code['name'], city['name'])
+        print('Shipping {}'.format(shipping))
+        # POST
+        cart = self.client.post('/api/shipping/confirm/{}'.format(uniqueid), json=shipping, headers={'x-forwarded-for': fake_ip}).json()
+        print('Final cart {}'.format(cart))
+
+        order = self.client.post('/api/payment/pay/{}'.format(uniqueid), json=cart, headers={'x-forwarded-for': fake_ip}).json()
+        print('Order {}'.format(order))
 
     @task
     def error(self):
-        if 'ERROR' in os.environ and os.environ['ERROR'] == '1':
+        fake_ip = random.choice(self.fake_ip_addresses)
+        if os.environ.get('ERROR') == '1':
             print('Error request')
             cart = {'total': 0, 'tax': 0}
-            self.client.post('/api/payment/pay/partner-57', json=cart, name='/api/payment/pay')
-
-
-   
+            self.client.post('/api/payment/pay/partner-57', json=cart, headers={'x-forwarded-for': fake_ip})
