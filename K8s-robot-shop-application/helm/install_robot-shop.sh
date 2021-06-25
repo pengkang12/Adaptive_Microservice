772ac32010afbc090bb7749addc23a2800b3e80b
tutu <123kangpeng@gmail.com>
2021-06-25 16:32:13 -0500

    update

--- K8s-robot-shop-application/helm/install_robot-shop.sh
+++ K8s-robot-shop-application/helm/install_robot-shop.sh
@@ -1,3 +1,4 @@
 helm uninstall robot-shop --namespace robot-shop 
+sleep 15
 
 helm install robot-shop --namespace robot-shop .
--- collecting_data/lib/locust_basic.py
+++ collecting_data/lib/locust_basic.py
@@ -2,12 +2,10 @@ import os
 
 def moveLocustResults(testDirPath):
     workingDir = os.getcwd()
-    file_list = ["locust_exceptions.csv", "locust_stats.csv","locust_failures.csv"]
-    for name in file_list:
-        locust_stats = "locust_stats.csv"
-        if os.path.isfile(os.path.join(workingDir , locust_stats)):
-            if not os.path.exists(testDirPath):
-                os.makedirs(testDirPath)
-            os.rename(os.path.join(workingDir, locust_stats), os.path.join(testDirPath, locust_stats))
-        else:
-            print("Unable to find locust_distribution.csv for destination folder %s\n", testDirPath)
+    locust_stats = "locust_stats.csv"
+    if os.path.isfile(os.path.join(workingDir , locust_stats)):
+        if not os.path.exists(testDirPath):
+            os.makedirs(testDirPath)
+        os.rename(os.path.join(workingDir, locust_stats), os.path.join(testDirPath, locust_stats))
+    else:
+        print("Unable to find locust_distribution.csv for destination folder %s\n", testDirPath)
